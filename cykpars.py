67ae24293eea1d096cc177a20e2b3c5c80fb75cc
tim <jackouttabox@gmail.com>
2017-01-04 01:37:47 +0100

    third times the charm

--- cykpars.py
+++ cykpars.py
@@ -21,105 +21,6 @@ def initArray(WORD):
 	tArray = np.zeros([height, length], list)
 	return tArray
 
-
-def findFinalRules(charItem):
-	rulesFound = []
-	#charItem: ['CB, CF']
-	#rule: AB
-	print(len(charItem))
-	for i,j in range(len(charItem)):
-		for h,k in rules.items():
-			for charItem in j:
-				for rule in k:
-					if rule == charItem:
-						rulesFound.append(i)
-	return rulesFound
-
-def parse(WORD, array, wHeight, u):
-	#print(u)
-	print("------------FUNCTION START----------------")
-	print(array)
-	possibleProductions = []
-	length = len(WORD)
-	length = (length - u) 
-	if(u == 0):
-		return array
-	else:
-		#CHECK LOOP NEST IF CORRECT
-		for i in range(length):
-			possibleProductions = []
-			inDown = u - 1
-			for z in range(1, wHeight):
-				
-				if(u == 0):
-					dummy = 9
-				elif(inDown < 0):
-					dummy = 9
-				else:
-					#print("I = ", i, "inDown= ", inDown)
-
-					tempC = array[inDown][i]
-					if(tempC == 0):
-						dummy = 9
-					else:
-						print("tempC: ", tempC, "an Stelle Height:", inDown, "Right: ", i)
-						#array[inDown][i] = ['H']
-						#print(tempC)
-						possibleProductions.append(tempC)
-						print("Possible Production:", possibleProductions)
-						#straight downwards
-						inDown = inDown - 1 
-			for k in range(1, wHeight):
-				combList = []
-				indexList = 0
-				inRight = u 
-				inHeight = i
-				tempD = array[inHeight][inRight]
-				print("Diagonal tempD: ", tempD )
-				if(tempD == 0):
-					dummy = 10
-				else:
-					for g in range(len(possibleProductions)):
-						indexG = np.size(possibleProductions[g])
-						
-						indexD = len(tempD) 
-						for h in range(len(tempD)):
-							for o in range(indexG):
-								if(tempD[h] == 0):
-									dummy = 9
-								else:
-
-									print(possibleProductions[g][o])
-									print(tempD[h])
-									checkC = ''.join(item for item in possibleProductions[g][o] + tempD[h])
-									#checkC = ''.join(item for item in posList[u][1] + temD[i])
-									combList.append(checkC)
-				
-									#checkC = tempC.join(possibleProductions[indexList])
-									#checkC = possibleProductions[indexList] + tempC
-									#ALT:
-									#checkC = ''.join(item for item in possibleProductions[indexList] + tempD)
-
-									#for o in range()
-									#checkC = possibleProductions[indexList][0] + tempC[0]
-									#print("CheckC: ", checkC)
-									#foundC = findRules(checkC)
-									print("--comblist-")
-									print(combList)
-									foundC = findFinalRules(combList)
-									print("foundC: ", foundC)
-									array[u][i] = foundC
-				#Diagonal upwards
-				inRight = inRight - 1
-				inHeight = inHeight + 1
-				indexList = indexList + 1
-
-
-
-	print("------------FUNCTION END----------------")
-	return array
-
-
 def parseFirst(WORD, array):
 	possibleProductions = {}
 	length = len(WORD)
@@ -135,24 +36,23 @@ def parseFirst(WORD, array):
 		if(len(foundC) == 0):
 			print("Word cant be build")
 			print(array)
-			return 1
+			#return 1
 	#print(array)
 	return array
 
-
-
-
+def parse(WORD, array, i):
+	length = len(WORD)
+	height = length
+	print(i)
+	return array
 
 def main():
 	WORD = input("Enter the word to test \n")
 	array = initArray(WORD)
 	array = parseFirst(WORD, array)
-	#loop:
-	wHeight = len(WORD)
-	for u in range(1, wHeight):
-		array = parse(WORD, array, wHeight, u)
-	print(array)
-
+	for i in range(1, len(WORD)):
+		array = parse(WORD, array, i)
+		print(array)
 
 if __name__ == "__main__":
 	main()
--- listTest.py
+++ listTest.py
@@ -1,23 +1,100 @@
 import numpy as np 
 import os
-
-def findRules(charItem):
-	#Empty list to collect the rules found for the
-	#Specific charItem
-	rulesFound = []
-	#Iterate trough rules from grammar.py
-	for i,j in rules.items():
-		for rule in j:
-			#if a rule is found add it to the end of the list
-			#Double check for multiple rules
-			if rule == charItem:
-				rulesFound.append(i)
-	return rulesFound
+from grammar import rules
 
 
 
-max = 1
-indexList = 0
+
+
+
+
+array = np.zeros([4, 4], int)
+array[1][3] = 1
+print(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def findFinalRules(charItem):
+#	rulesFound = []
+#	#charItem: ['CB, CF']
+#	#rule: AB
+#	print(len(charItem))
+#	print("--comblist-")
+#	print(charItem)
+#	print(len(charItem))
+#	for f in range(len(charItem)):
+#		tempS = charItem[f]
+#		print(tempS)
+#		
+#		for h,k in rules.items():
+#			for rule in k:
+#				if rule == tempS:
+#					rulesFound.append(h)
+#
+#	return rulesFound
+
+
+#combList = ['CD']
+#print("combList: ", combList)
+#foundC = findFinalRules(combList)
+#print("----------")
+#print(foundC)
+#posList=[]
+#tmpC = ['E', 'R']
+
+
+#max = 1
+#indexList = 0
 
 #possible: [['A'], ['C']]
 #tempD: ['E', 'B']
@@ -31,25 +108,27 @@ indexList = 0
 #mylist = [1,2,3,4]
 #print len(mylist)
 #4
-
-combList = []
-posList=[['A', 'J'], ['C']]
-tmpC = ['E', 'R']
+#cbcb
+#B,E | D | B,E | D
 
 
-for u in range(len(posList)):
-	indexG = len(posList[u])
-	indexD = len(tmpC) 
-	for i in range(len(tmpC)):
-		for o in range(indexG):
-			tempJ = posList[u][o]
-			tempK = tmpC[i] 
-			#print("j", tempJ, "k", tempK)
-			checkC = ''.join(item for item in posList[u][o] + tmpC[i])
-			#checkC = ''.join(item for item in posList[u][1] + tmpC[i])
-			combList.append(checkC)
-			
 
+#for u in range(len(posList)):
+#	indexG = len(posList[u])
+#	indexD = len(tmpC) 
+#	for i in range(len(tmpC)):
+#		for o in range(indexG):
+#			tempJ = posList[u][o]
+#			tempK = tmpC[i] 
+#			#print("j", tempJ, "k", tempK)
+#			checkC = ''.join(item for item in posList[u][o] + tmpC[i])
+#			#checkC = ''.join(item for item in posList[u][1] + tmpC[i])
+#			combList.append(checkC)
+#			print(combList)
+#			foundC = findFinalRules(combList)
+#			print(foundC)
+#			
+#
 #print(tmpC[1])
 #checkC = ''.join(item for item in posList[indexList] + tmpC)
-print(combList)
+#print(combList)
