8a665ba96fed86a2b7024c9a46156a9feb07a9a2
Tim <jackouttabox@gmail.com>
2017-01-05 19:34:30 +0100

    checking diag working

--- cykpars.py
+++ cykpars.py
@@ -41,36 +41,48 @@ def parseFirst(tWORD, array):
 	#print(array)
 	return array
 
-def getDiag(array, possibleProductions, currentLength, currentHeight):
+
+def getDiag(array, possibleProductions, currentLength, currentHeight, length, i):
 	#Get Diagonal item from array
 	#check with possible productions
-	
-
-
-
-
-
-
-
+	tempList = []
+	#Diag length is the current height + the current length
+	indexDiagLenght = currentHeight + currentLength
+	#Diag always starts at Height 0 
+	indexDiagHeight = 0
+	print("currentHeight", currentHeight)
+	for diagLoopIndex in range(currentHeight):
+		print("STELLE: ", indexDiagHeight, "|", indexDiagLenght)
+		print(array[indexDiagHeight][indexDiagLenght])
+		tempDiag = array[indexDiagHeight][indexDiagLenght]
+		tempList.append(tempDiag)
+		#MOVE DIAG LEFT UP
+		indexDiagLenght = indexDiagLenght - 1
+		indexDiagHeight = indexDiagHeight + 1
+		
+	print("Loop finished Diag list:")
+	print(tempList)
+	#COMBINE POSSIBLELIST + TEMPLIST AND CHECK IF RULE IS FOUD
 	return array
 
 
 #MIGHT BE FINE
 def getDown(array, possibleProductions, currentHeight, currentLength):
-	print("getDown")
+	#print("getDown")
 
 	for indexHeight in range(currentHeight):
 		#if 0 dann ['0'] oder so
-		print("tempSignal= ", indexHeight, "|", currentLength)
+		#print("tempSignal= ", indexHeight, "|", currentLength)
 		tempSignal = array[indexHeight][currentLength]
 		if(tempSignal == 0):
+			possibleProductions.append(['EMPTY'])
 			print("No Production, ignore")
 		else:
-			print("tempSignal", tempSignal)
-			print("array:", array[indexHeight][currentLength])
+			#print("tempSignal", tempSignal)
+			#print("array:", array[indexHeight][currentLength])
 			possibleProductions.append(tempSignal)
 	possibleProductions.reverse()
-	print("---EXIT GETDOWN")
+	#print("---EXIT GETDOWN")
 	return possibleProductions
 
 
@@ -80,14 +92,14 @@ def parse(tWORD, array, i):
 	currentHeight = i
 	#The algorithm doesnt need to check every field in the array
 	widthLength = length - i
-	print("currentHeight", currentHeight)
+	#print("currentHeight", currentHeight)
 	for currentLength in range(widthLength):
-		print("----FOR LOOP----")
-		print("currentLength", currentLength)
+		#print("----FOR LOOP----")
+		print("CHECKING HEIGHT: ",currentHeight, "LENGHTH:",  currentLength)
 		possibleProductions = []
 		possibleProductions = getDown(array, possibleProductions, currentHeight, currentLength)
-		print("possible:", possibleProductions)
-		array = getDiag(array, possibleProductions, currentLength, currentHeight)
+		#print("possible:", possibleProductions)
+		array = getDiag(array, possibleProductions, currentLength, currentHeight, length, i)
 	
 	return array
 
@@ -101,9 +113,11 @@ def main():
 	#WORD = input("Enter the word to test \n")
 	array = initArray(tWORD)
 	array = parseFirst(tWORD, array)
-	#array[1][0] = ['G']
+	#
+	array[1][2] = ['G']
+	array[2][1] = ['K']
 	for i in range(1, len(tWORD)):
-		print("main loop:", i)
+		#print("main loop:", i)
 		array = parse(tWORD, array, i)
 	print(array)
 
