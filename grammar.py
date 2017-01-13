79e7443f48d1c3c9731f2a04d2ab0b1a8dc99f55
tim <jackouttabox@gmail.com>
2017-01-13 15:43:30 +0100

    Added rules

--- grammar.py
+++ grammar.py
@@ -6,7 +6,7 @@
 #E -> c
 #F -> AD
 #Word: aaabbbcc
-rules = {'S': ['AB'],'A': ['CD','CF'], 'B': ['c','EB'], 'C': ['a'], 'D' : ['b'], 'E' : ['c'], 'F' : ['AD']}
+#rules = {'S': ['AB'],'A': ['CD','CF'], 'B': ['c','EB'], 'C': ['a'], 'D' : ['b'], 'E' : ['c'], 'F' : ['AD']}
 
 #S -> AB|BC
 #A -> BA|a
@@ -14,3 +14,11 @@ rules = {'S': ['AB'],'A': ['CD','CF'], 'B': ['c','EB'], 'C': ['a'], 'D' : ['b'],
 #C -> AB|a
 #Word:baaba
 #rules = {'S': ['AB', 'BC'],'A': ['BA','a'], 'B': ['b','CC'], 'C': ['a', 'AB']}
+
+#rules = {'S':['AB', 'CD', 'AT', 'CU', 'SS'], 'T':['SB'], 'U':['SD'], 'A':['('], 'B':[')'], 'C':['['], 'D':[']']}
+
+#S -> BC
+#C -> SA
+#A -> a
+#B -> b
+rules = {'S':['BC'], 'C':['SA','BA'], 'A':['a'], 'B':['b']} 
